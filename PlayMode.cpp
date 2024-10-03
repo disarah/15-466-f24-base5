7d8a8b43d881a080b7934246bf84748d00495b47
Sarah Di <sarahdi@andrew.cmu.edu>
2024-10-03 12:13:50 -0400

    working mesh

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -145,7 +145,7 @@ void PlayMode::update(float elapsed) {
 	//player walking:
 	{
 		//combine inputs into a move:
-		constexpr float PlayerSpeed = 1.0f;
+		constexpr float PlayerSpeed = 3.0f;
 		glm::vec2 move = glm::vec2(0.0f);
 		if (left.pressed && !right.pressed) move.x =-1.0f;
 		if (!left.pressed && right.pressed) move.x = 1.0f;
